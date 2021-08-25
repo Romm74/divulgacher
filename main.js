7f7cc8c397c88f88713eaa3494bd7cfe9753c2ba
Romain <monaco.rom1@gmail.com>
2021-08-25 16:10:27 +0200

    Ajout des image et de l'affichage de spoil

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-function changerCouleur (id, couleur)
-{
-  document.getElementById(id).style.color=couleur;
-}
+
+  document.getElementById("divulgacher").addEventListener("click", function(){
+      document.querySelector("#zoneDeDivulgachage").innerHTML = "SURPRISE ! Le bateau coule ... comme l'Abeille coule (à répéter 20 fois ! :) )"
+  });
+let films = 
+    ["JOKER", "Titanic", "Harry Potter", "Star Wars", "Amélie Poulain", ""];
+let spoilfilm = ["Le joker est ennuyant", "Le bateau coule", "Yoda n'as plus de force", ""];
